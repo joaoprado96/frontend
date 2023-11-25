8d689308475a5ac43e2a71200c2ca38da291515e
João Prado <58610872+joaoprado96@users.noreply.github.com>
2023-11-24 23:01:29 -0300

    atualização

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { LugaresComponent } from './lugares/lugares.component';
+import { LugarService } from './lugar.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LugaresComponent
+    // outros componentes aqui
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
+    // outros módulos aqui
   ],
-  providers: [],
+  providers: [LugarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
