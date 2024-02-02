a8a5e3159c477cac9a0991edd492d89c1c6f8346
João Prado <58610872+joaoprado96@users.noreply.github.com>
2024-02-02 09:29:53 -0300

    Atualização de propaganda

--- acheai/src/pages/home.js
+++ acheai/src/pages/home.js
@@ -2,78 +2,43 @@ import React, { useState, useEffect } from 'react';
 import { Carousel } from 'react-bootstrap';
 import Head from 'next/head';
 import Link from 'next/link'; // Importe o componente Link aqui
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
-    const [destaques, setDestaques] = useState([]);
-
-    useEffect(() => {
-        carregarFotos();
-        // Outras lógicas que você precisa executar no carregamento
-    }, []);
-
-    const carregarFotos = async () => {
-        try {
-            const lugaresResponse = await fetch('https://application-backend.onrender.com/api/destaques/lugares');
-            const lugares = await lugaresResponse.json();
-            if (lugares && lugares.length > 0) {
-                const randomIndex = Math.floor(Math.random() * lugares.length);
-                const lugarIdSelecionado = lugares[randomIndex];
-
-                const fotosResponse = await fetch(`https://application-backend.onrender.com/api/fotos-destaques/${lugarIdSelecionado}`);
-                const fotos = await fotosResponse.json();
-                setDestaques(fotos);
-            } else {
-                throw new Error('Nenhum lugar encontrado.');
-            }
-        } catch (error) {
-            console.error('Erro ao carregar fotos:', error);
-        }
-    };
-
-    const bufferToBase64 = (buf) => {
-        var binstr = Array.prototype.map.call(buf, function (ch) {
-            return String.fromCharCode(ch);
-        }).join('');
-        return btoa(binstr);
-    };
-
-
-    return (
-        <>
-            <Head>
-                <title>AcheAi</title>
-                <link rel="icon" type="image/png" href="/icons/logo.png" />
-                <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
-            </Head>
-            {/* Substitua pelo componente de Carrossel se necessário */}
-            <div id="carouselNovidades" className="carousel-fullscreen"></div>
-
-            <div className="page">
-              {destaques.length > 0 && (
-                  <Carousel>
-                      {destaques.map((destaque, index) => (
-                          <Carousel.Item key={index}>
-                              <img
-                                  className="d-block w-100"
-                                  src={`data:${destaque.contentType};base64,${bufferToBase64(destaque.data.data)}`}
-                                  alt={`Slide ${index}`}
-                              />
-                          </Carousel.Item>
-                      ))}
-                  </Carousel>
-              )}
-                <div className="container-central">
-                    <img src="icons/logo.png" alt="Ache Aí" width="300" height="300" />
-                    <h2 id="manchete" className="h2-titulo">Onde você quer ir hoje?</h2>
-                    <br />
-                    <p className="p-titulo">O melhor da gastronomia e entretenimento em um só lugar.</p>
-                    <br />
-                    <Link href="/estabelecimentos">
-                        <p className="b-titulo">Encontrar</p>
-                    </Link>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>AcheAi</title>
+        <link rel="icon" type="image/png" href="/icons/logo.png" />
+      </Head>
+      <div className="video-background">
+        <video
+          autoPlay
+          loop
+          muted
+          style={{
+            position: "fixed", // Use fixed para cobrir toda a tela, ou absolute dentro de um container específico
+            width: "100%",
+            left: "50%",
+            top: "50%",
+            height: "100%",
+            objectFit: "cover",
+            transform: "translate(-50%, -50%)",
+            zIndex: "-1", // Garante que o vídeo fique atrás de todo conteúdo
+          }}
+          src="/image/propaganda.mp4"
+        >
+          Seu navegador não suporta o elemento de vídeo.
+        </video>
+      </div>
+      <div className="page">
+        <div className="container-central" style={{position: 'relative', zIndex: 2}}> {/* Garanta que esteja acima do vídeo */}
+          {/* O seu conteúdo aqui */}
+          <img src="icons/logo.png" alt="Ache Aí" width="300" height="200" />
+          <h2 id="manchete" className="h2-titulo">Onde você quer ir hoje?</h2>
+          <p className="p-titulo">O melhor da gastronomia e entretenimento em um só lugar.</p>
+          <Link href="/estabelecimentos"><p className="b-titulo">Encontrar</p></Link>
+        </div>
+      </div>
+    </>
+  );
 }
