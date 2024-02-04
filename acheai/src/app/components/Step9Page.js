22d9d999458ac2a9e0cad923f0061a228fc3193a
João Prado <58610872+joaoprado96@users.noreply.github.com>
2024-02-04 16:02:28 -0300

    Atualização de Cadastro

--- acheai/src/app/components/Step9Page.js
+++ acheai/src/app/components/Step9Page.js
@@ -1,9 +1,9 @@
-import { useProgress } from '../../app/contexts/ProgressContext';
+import { useProgress } from '../contexts/ProgressContext';
 import Head from 'next/head';
 import React from 'react';
 import Select from 'react-select';
 
-const Step1Page = () => {
+const Step9Page = () => {
   const { currentStep, handleNextStep, handlePreviousStep } = useProgress();
   return (
     <>
@@ -12,14 +12,13 @@ const Step1Page = () => {
     </Head>
     <div className='formulario-cadastro'>
       <br/><br/><br/>
-      <h1>Passo 1: Termos e Condições</h1>
+      <h1>Passo 9: Confirmação de Cadastro</h1>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '5px' }}>
         <button className="btn mt-3" onClick={handlePreviousStep}>Anterior</button>
-        <button className="btn mt-3" onClick={handleNextStep}>Próximo</button>
       </div>
     </div>
     </>
   );
 };
 
-export default Step1Page;
+export default Step9Page;
--- acheai/src/pages/cadastro/index.js
+++ acheai/src/pages/cadastro/index.js
@@ -1,12 +1,65 @@
-import { ProgressProvider } from '../../app/contexts/ProgressContext';
+import React from 'react';
+import { ProgressProvider, useProgress } from '../../app/contexts/ProgressContext';
+import ProgressBar from '../../app/components/ProgressBar'; // Verifique o caminho para o seu ambiente
 import Step1Page from '../../app/components/Step1Page';
+import Step2Page from '../../app/components/Step2Page';
+import Step3Page from '../../app/components/Step3Page';
+import Step4Page from '../../app/components/Step4Page';
+import Step5Page from '../../app/components/Step5Page';
+import Step6Page from '../../app/components/Step6Page';
+import Step7Page from '../../app/components/Step7Page';
+import Step8Page from '../../app/components/Step8Page';
+import Step9Page from '../../app/components/Step9Page';
+
+import { useRouter } from 'next/router';
 
 const Cadastro = () => {
+  const router = useRouter();
+  const { step } = router.query; // Obtém o valor do parâmetro 'step' da URL
+
+  // Ajusta o valor de 'step' para 1 se for 'undefined' ou não puder ser convertido em um número
+  const currentStep = parseInt(step) || 1;
+
+  let content;
+
+  switch (currentStep) {
+    case 1:
+        content = <Step1Page />;
+        break;
+    case 2:
+        content = <Step2Page />;
+        break;
+    case 3:
+        content = <Step3Page />;
+        break;
+    case 4:
+        content = <Step4Page />;
+        break;
+    case 5:
+        content = <Step5Page />;
+        break;
+    case 6:
+        content = <Step6Page />;
+        break;
+    case 7:
+        content = <Step7Page />;
+        break;
+    case 8:
+        content = <Step8Page />;
+        break;
+    case 9:
+        content = <Step9Page />;
+        break;
+    default:
+        content = <div><br/><br/><br/><br/><br/>Página não encontrada</div>;
+  }
+
   return (
     <ProgressProvider>
-      <Step1Page />
+      <ProgressBar totalSteps={9} />
+      {content}
     </ProgressProvider>
   );
-};
+}
 
 export default Cadastro;
